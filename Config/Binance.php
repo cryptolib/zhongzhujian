6dc5ecdcc8074c6a0b2b57686ff4fda9a086c9d8
梁敏成 <yeah@minch.me>
2020-05-27 23:17:48 +0800

    init project

---
+++ Config/Binance.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Config;
+
+/**
+ * Binance接口配置
+ * @author minch
+ */
+class Binance
+{
+    /**
+     * API KEY
+     * @var string
+     */
+    public static $api_key = 'PFnZzFG3BuEjixBkYUHKvFPBBhbl9CJ28lqpET8oX93qgfbulEpP43j77pLmYhJD';
+
+    /**
+     * 加密密钥
+     * @var string
+     */
+    public static $api_secret = 'OJMGRALLVial4JqJRk9vcBALXJFL8oO6dEzxHCBmNdtFwwMMtbyRv2b7vUEJyaIO';
+}
